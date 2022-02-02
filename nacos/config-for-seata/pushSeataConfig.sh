fdfbefabbf1a80f11dfdae7c1311849c6c03c7fb
nieyinliang <nieyinliang@xiaomi.com>
2022-02-03 05:32:16 +0800

    bug fix

--- nacos/config-for-seata/pushSeataConfig.sh
+++ nacos/config-for-seata/pushSeataConfig.sh
@@ -6,7 +6,7 @@ echo "###########################################################"
 echo "###########################################################"
 echo "###########################################################"
 # use while loop to check if nacos is running
-nacosStatuCheckUrl="http://${HOST_IP}:${COMMON_NACOS_PORT}/nacos/v1/cs/configs?dataId=${NACOS_STATUS_CHECK_KEY}&tenant=${COMMON_NACOS_NAMESPACE}&group=${COMMON_NACOS_GROUP}}"
+nacosStatuCheckUrl="http://${HOST_IP}:${COMMON_NACOS_PORT}/nacos/v1/cs/configs?dataId=${NACOS_STATUS_CHECK_KEY}&tenant=${COMMON_NACOS_NAMESPACE}&group=${COMMON_NACOS_GROUP}"
 echo "nacosStatuCheckUrl : ${nacosStatuCheckUrl}"
 
 while true
@@ -16,7 +16,12 @@ do
     if [ 0 = $verifier ]
         then
             echo "synchronizing seata config to nacos"
-            sh /home/seata/nacos-config.sh -h 127.0.0.1 -p 8848 -u nacos -w nacos -t ${COMMON_NACOS_NAMESPACE} -g ${COMMON_NACOS_GROUP}
+            if [ "$COMMON_NACOS_NAMESPACE" =  "" ]
+                then
+                    sh /home/seata/nacos-config.sh -h 127.0.0.1 -p 8848 -u nacos -w nacos -g ${COMMON_NACOS_GROUP}
+                else
+                    sh /home/seata/nacos-config.sh -h 127.0.0.1 -p 8848 -u nacos -w nacos -g ${COMMON_NACOS_GROUP} -t ${COMMON_NACOS_NAMESPACE}
+            fi
             break
         else
             echo "nacos is not running yet"
